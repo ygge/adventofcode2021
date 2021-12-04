71bc535decc13623d2fe80611060c09a02820b4d
Henrik Ygge <henrik@meepo.se>
2021-12-04 06:36:41 +0100

    clean up day 4

--- src/main/java/Day4.java
+++ src/main/java/Day4.java
@@ -1,7 +1,6 @@
 import util.Util;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -18,21 +17,7 @@ public class Day4 {
         List<Integer> nums = Stream.of(inputs.get(0).split(","))
             .map(Integer::parseInt)
             .collect(Collectors.toList());
-        List<int[][]> boards = new ArrayList<>();
-        for (int i = 2; i < inputs.size(); i += 6) {
-            int[][] board = new int[5][5];
-            for (int j = 0; j < 5; ++j) {
-                var s = inputs.get(i+j).split(" ");
-                board[j] = new int[5];
-                int k = 0;
-                for (String s1 : s) {
-                    if (s1.length() > 0) {
-                        board[j][k++] = Integer.parseInt(s1);
-                    }
-                }
-            }
-            boards.add(board);
-        }
+        List<int[][]> boards = parseBoard(inputs);
         boolean[] done = new boolean[boards.size()];
         int numDone = 0;
         for (Integer num : nums) {
@@ -43,10 +28,6 @@ public class Day4 {
                 int res = checkBoard(boards.get(a), num);
                 if (res != -1) {
                     done[a] = true;
-                    System.out.println("Solved board " + a + "!");
-                    for (int y = 0; y < boards.get(a).length; ++y) {
-                        System.out.println(Arrays.toString(boards.get(a)[y]));
-                    }
                     if (++numDone == boards.size()) {
                         return res;
                     }
@@ -54,6 +35,41 @@ public class Day4 {
             }
         }
         throw new IllegalStateException();
+    }
+
+    private static int part1(List<String> inputs) {
+        List<Integer> nums = Stream.of(inputs.get(0).split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        List<int[][]> boards = parseBoard(inputs);
+        for (Integer num : nums) {
+            for (int[][] board : boards) {
+                int score = checkBoard(board, num);
+                if (score != -1) {
+                    return score;
+                }
+            }
+        }
+        throw new IllegalStateException();
+    }
+
+    private static List<int[][]> parseBoard(List<String> inputs) {
+        List<int[][]> boards = new ArrayList<>();
+        for (int i = 2; i < inputs.size(); i += 6) {
+            int[][] board = new int[5][5];
+            for (int j = 0; j < 5; ++j) {
+                var s = inputs.get(i + j).split(" ");
+                board[j] = new int[5];
+                int k = 0;
+                for (String s1 : s) {
+                    if (s1.length() > 0) {
+                        board[j][k++] = Integer.parseInt(s1);
+                    }
+                }
+            }
+            boards.add(board);
+        }
+        return boards;
     }
 
     private static int checkBoard(int[][] board, int num) {
@@ -113,87 +129,6 @@ public class Day4 {
             }
         }
         return -1;
-    }
-
-    private static int part1(List<String> inputs) {
-        List<Integer> nums = Stream.of(inputs.get(0).split(","))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-        List<int[][]> boards = new ArrayList<>();
-        for (int i = 2; i < inputs.size(); i += 6) {
-            int[][] board = new int[5][5];
-            for (int j = 0; j < 5; ++j) {
-                var s = inputs.get(i+j).split(" ");
-                board[j] = new int[5];
-                int k = 0;
-                for (String s1 : s) {
-                    if (s1.length() > 0) {
-                        board[j][k++] = Integer.parseInt(s1);
-                    }
-                }
-            }
-            boards.add(board);
-        }
-        for (Integer num : nums) {
-            for (int[][] board : boards) {
-                for (int y = 0; y < board.length; ++y) {
-                    for (int x = 0; x < board[y].length; ++x) {
-                        if (board[y][x] == num) {
-                            board[y][x] = -1;
-                        }
-                    }
-                }
-                for (int y = 0; y < board.length; ++y) {
-                    boolean solved = true;
-                    for (int x = 0; x < board[y].length; ++x) {
-                        if (board[y][x] != -1) {
-                            solved = false;
-                            break;
-                        }
-                    }
-                    if (solved) {
-                        return score(num, board);
-                    }
-                }
-                for (int x = 0; x < board[0].length; ++x) {
-                    boolean solved = true;
-                    for (int y = 0; y < board.length; ++y) {
-                        if (board[x][y] != -1) {
-                            solved = false;
-                            break;
-                        }
-                    }
-                    if (solved) {
-                        return score(num, board);
-                    }
-                }
-                for (int i = 0; i < board.length; ++i) {
-                    boolean solved = true;
-                    for (int y = 0; y < board.length; ++y) {
-                        if (board[y][y] != -1) {
-                            solved = false;
-                            break;
-                        }
-                    }
-                    if (solved) {
-                        return score(num, board);
-                    }
-                }
-                for (int i = 0; i < board.length; ++i) {
-                    boolean solved = true;
-                    for (int y = 0; y < board.length; ++y) {
-                        if (board[y][board[y].length-i-1] != -1) {
-                            solved = false;
-                            break;
-                        }
-                    }
-                    if (solved) {
-                        return score(num, board);
-                    }
-                }
-            }
-        }
-        throw new IllegalStateException();
     }
 
     private static int score(Integer num, int[][] board) {
