beb15160805a7e878f17761bc006d26ec317f651
Henrik Ygge <henrik@meepo.se>
2021-12-05 06:22:00 +0100

    clean up day 5

--- src/main/java/Day5.java
+++ src/main/java/Day5.java
@@ -12,23 +12,17 @@ public class Day5 {
     }
 
     private static int part2(List<String> inputs) {
-        Map<Pos, Integer> seen = new HashMap<>();
-        inputs.stream()
-                .map(Day5::parse2)
-                .flatMap(Collection::stream)
-                .forEach(pos -> {
-                    var prev = seen.get(pos);
-                    seen.put(pos, prev == null ? 1 : prev + 1);
-                });
-        return (int)seen.values().stream()
-                .filter(v -> v > 1)
-                .count();
+        return countDuplicates(inputs, true);
     }
 
     private static int part1(List<String> inputs) {
+        return countDuplicates(inputs, false);
+    }
+
+    private static int countDuplicates(List<String> inputs, boolean considerDiagonals) {
         Map<Pos, Integer> seen = new HashMap<>();
         inputs.stream()
-                .map(Day5::parse1)
+                .map(row -> parse(row, considerDiagonals))
                 .filter(Objects::nonNull)
                 .flatMap(Collection::stream)
                 .forEach(pos -> {
@@ -40,7 +34,7 @@ public class Day5 {
                 .count();
     }
 
-    private static List<Pos> parse1(String row) {
+    private static List<Pos> parse(String row, boolean considerDiagonals) {
         var s = row.split(" -> ");
         var s1 = s[0].split(",");
         int x1 = Integer.parseInt(s1[0]);
@@ -59,38 +53,16 @@ public class Day5 {
             List<Pos> pos = new ArrayList<>();
             for (int i = Math.min(x1, x2); i <= Math.max(x1, x2); ++i) {
                 pos.add(new Pos(i, y1));
+            }
+            return pos;
+        }
+        if (considerDiagonals) {
+            List<Pos> pos = new ArrayList<>();
+            for (int i = 0; i <= Math.max(x1, x2)-Math.min(x1, x2); ++i) {
+                pos.add(new Pos(x1+(x1 < x2 ? 1 : -1)*i, y1+(y1 < y2 ? 1 : -1)*i));
             }
             return pos;
         }
         return null;
-    }
-
-    private static List<Pos> parse2(String row) {
-        var s = row.split(" -> ");
-        var s1 = s[0].split(",");
-        int x1 = Integer.parseInt(s1[0]);
-        int y1 = Integer.parseInt(s1[1]);
-        var s2 = s[1].split(",");
-        int x2 = Integer.parseInt(s2[0]);
-        int y2 = Integer.parseInt(s2[1]);
-        if (x1 == x2) {
-            List<Pos> pos = new ArrayList<>();
-            for (int i = Math.min(y1, y2); i <= Math.max(y1, y2); ++i) {
-                pos.add(new Pos(x1, i));
-            }
-            return pos;
-        }
-        if (y1 == y2) {
-            List<Pos> pos = new ArrayList<>();
-            for (int i = Math.min(x1, x2); i <= Math.max(x1, x2); ++i) {
-                pos.add(new Pos(i, y1));
-            }
-            return pos;
-        }
-        List<Pos> pos = new ArrayList<>();
-        for (int i = 0; i <= Math.max(x1, x2)-Math.min(x1, x2); ++i) {
-            pos.add(new Pos(x1+(x1 < x2 ? 1 : -1)*i, y1+(y1 < y2 ? 1 : -1)*i));
-        }
-        return pos;
     }
 }
