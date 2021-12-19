dec2d080817b75a23ea1be0f2d18f73dd28d9e61
Henrik Ygge <henrik@meepo.se>
2021-12-19 07:32:19 +0100

    solved day 19

--- src/main/java/Day19.java
+++ src/main/java/Day19.java
@@ -1,16 +1,48 @@
 import util.Util;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Day19 {
 
     public static void main(String[] args) {
         var inputs = Util.readStrings();
-        Util.submitPart1(part1(inputs));
+        solve(inputs);
     }
 
-    private static int part1(List<String> inputs) {
+    private static void solve(List<String> inputs) {
+        List<List<Pos>> scanners = parseScanners(inputs);
+
+        List<Pos> scannerPos = new ArrayList<>();
+        scannerPos.add(new Pos(0, 0, 0));
+        Set<Pos> beacons = new HashSet<>(scanners.remove(0));
+        while (scanners.size() > 0) {
+            boolean anyMatch = false;
+            int index = 0;
+            for (; index < scanners.size(); ++index) {
+                if (match(beacons, scanners.get(index), scannerPos)) {
+                    scanners.remove(index);
+                    anyMatch = true;
+                    --index;
+                }
+            }
+            if (!anyMatch) {
+                throw new RuntimeException("Could not match any scanner");
+            }
+        }
+        Util.submitPart1(beacons.size());
+
+        int max = 0;
+        for (int i = 0; i < scannerPos.size(); ++i) {
+            for (int j = i+1; j < scannerPos.size(); ++j) {
+                max = Math.max(max, scannerPos.get(i).dist(scannerPos.get(j)));
+            }
+        }
+        Util.submitPart2(max);
+    }
+
+    private static List<List<Pos>> parseScanners(List<String> inputs) {
         List<List<Pos>> scanners = new ArrayList<>();
         for (int i = 0; i < inputs.size(); ++i) {
             if (inputs.get(i).charAt(0) == '-') {
@@ -23,50 +55,31 @@ public class Day19 {
                 scanners.add(beacons);
             }
         }
-
-        Set<Pos> beacons = new HashSet<>(scanners.remove(0));
-        while (scanners.size() > 0) {
-            boolean anyMatch = false;
-            int index = 0;
-            for (; index < scanners.size(); ++index) {
-                if (match(beacons, scanners.get(index))) {
-                    scanners.remove(index);
-                    System.out.println("Matched index " + index);
-                    anyMatch = true;
-                    --index;
-                }
-            }
-            if (!anyMatch) {
-                throw new RuntimeException("Could not match any scanner");
-            }
-        }
-
-        return beacons.size();
+        return scanners;
     }
 
-    private static boolean match(Set<Pos> beacons, List<Pos> pos) {
-        for (Pos beacon : beacons) {
-            for (int i = 0; i < 6; ++i) {
-                for (int j = 0; j < 4; ++j) {
-                    List<Pos> moved = move(pos, i, j);
-                    for (Pos p : moved) {
-                        List<Pos> mapped = new ArrayList<>();
-                        int dx = beacon.x - p.x;
-                        int dy = beacon.y - p.y;
-                        int dz = beacon.z - p.z;
-                        for (Pos pp : moved) {
-                            mapped.add(pp.move(dx, dy, dz));
+    private static boolean match(Set<Pos> beacons, List<Pos> pos, List<Pos> scannerPos) {
+        for (int i = 0; i < 24; ++i) {
+            List<Pos> moved = move(pos, i);
+            for (Pos beacon : beacons) {
+                for (Pos p : moved) {
+                    List<Pos> mapped = new ArrayList<>();
+                    int dx = beacon.x - p.x;
+                    int dy = beacon.y - p.y;
+                    int dz = beacon.z - p.z;
+                    for (Pos pp : moved) {
+                        mapped.add(pp.move(dx, dy, dz));
+                    }
+                    int count = 0;
+                    for (Pos pp : mapped) {
+                        if (beacons.contains(pp)) {
+                            ++count;
                         }
-                        int count = 0;
-                        for (Pos pp : mapped) {
-                            if (beacons.contains(pp)) {
-                                ++count;
-                            }
-                        }
-                        if (count >= 12) {
-                            beacons.addAll(mapped);
-                            return true;
-                        }
+                    }
+                    if (count >= 12) {
+                        beacons.addAll(mapped);
+                        scannerPos.add(new Pos(dx, dy, dz));
+                        return true;
                     }
                 }
             }
@@ -74,42 +87,28 @@ public class Day19 {
         return false;
     }
 
-    private static List<Pos> move(List<Pos> pos, int i, int j) {
-        if (i == 0) {
-            return move(pos.stream().map(p -> new Pos(p.x, p.y, p.z)).collect(Collectors.toList()), j);
+    private static List<Pos> move(List<Pos> pos, int j) {
+        List<Function<List<Pos>, List<Pos>>> actions = Arrays.asList(
+                Day19::roll, Day19::turn, Day19::turn, Day19::turn,
+                Day19::roll, Day19::turn, Day19::turn, Day19::turn,
+                Day19::roll, Day19::turn, Day19::turn, Day19::turn,
+                p -> roll(turn(roll(p))),
+                Day19::roll, Day19::turn, Day19::turn, Day19::turn,
+                Day19::roll, Day19::turn, Day19::turn, Day19::turn,
+                Day19::roll, Day19::turn, Day19::turn, Day19::turn
+        );
+        for (int i = 1; i <= j; ++i) {
+            pos = actions.get(i).apply(pos);
         }
-        if (i == 1) {
-            return move(pos.stream().map(p -> new Pos(p.y, -p.x, p.z)).collect(Collectors.toList()), j);
-        }
-        if (i == 2) {
-            return move(pos.stream().map(p -> new Pos(-p.x, -p.y, p.z)).collect(Collectors.toList()), j);
-        }
-        if (i == 3) {
-            return move(pos.stream().map(p -> new Pos(-p.y, p.x, p.x)).collect(Collectors.toList()), j);
-        }
-        if (i == 4) {
-            return move(pos.stream().map(p -> new Pos(p.z, p.x, p.y)).collect(Collectors.toList()), j);
-        }
-        if (i == 5) {
-            return move(pos.stream().map(p -> new Pos(p.z, p.y, p.x)).collect(Collectors.toList()), j);
-        }
-        throw new IllegalStateException();
+        return pos;
     }
 
-    private static List<Pos> move(List<Pos> pos, int j) {
-        if (j == 0) {
-            return pos.stream().map(p -> new Pos(p.x, p.y, p.z)).collect(Collectors.toList());
-        }
-        if (j == 1) {
-            return pos.stream().map(p -> new Pos(p.x, p.z, -p.y)).collect(Collectors.toList());
-        }
-        if (j == 2) {
-            return pos.stream().map(p -> new Pos(p.x, -p.y, -p.z)).collect(Collectors.toList());
-        }
-        if (j == 3) {
-            return pos.stream().map(p -> new Pos(p.x, -p.z, p.y)).collect(Collectors.toList());
-        }
-        throw new IllegalStateException();
+    private static List<Pos> roll(List<Pos> pos) {
+        return pos.stream().map(p -> new Pos(p.x, p.z, -p.y)).collect(Collectors.toList());
+    }
+
+    private static List<Pos> turn(List<Pos> pos) {
+        return pos.stream().map(p -> new Pos(-p.y, p.x, p.z)).collect(Collectors.toList());
     }
 
     private static class Pos {
@@ -142,7 +141,11 @@ public class Day19 {
         }
 
         public Pos move(int dx, int dy, int dz) {
-            return new Pos(x+dx, y+dy, z+dz);
+            return new Pos(x + dx, y + dy, z + dz);
+        }
+
+        public int dist(Pos pos) {
+            return Math.abs(x-pos.x) + Math.abs(y-pos.y) + Math.abs(z-pos.z);
         }
     }
 }
