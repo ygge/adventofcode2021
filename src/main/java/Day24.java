767dd1ab24cfdd8dae54e88ffbf5c8152c8ccbd2
Henrik Ygge <henrik@meepo.se>
2021-12-24 07:07:48 +0100

    solved day 24

---
+++ src/main/java/Day24.java
@@ -0,0 +1,18 @@
+public class Day24 {
+
+    public static void main(String[] args) {
+        //Löst för hand:
+        /*
+Om w5 = w4+1 ingen ändring på z
+Om w7 = w6+8-15 = w6-7 ingen ändring på z
+Om w10 = w9+8-13=w9-5
+Om w11 = w8+10-13=w8-3
+Om w12 = w3+6-14=w3-8
+Om w13 = w2+6-2=w2+4
+Om w14 = w1+14-9=w1+5
+
+störst: 459 899 299 461 99
+minst: 119 128 146 111 56
+         */
+    }
+}
